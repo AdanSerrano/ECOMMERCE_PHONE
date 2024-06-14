1295a513d0d719ee1432568dc82aaa7b0ac4fe90
addram <webmaster@fesa.com.pa>
2024-06-14 14:40:42 -0500

    feat(uploathing button added image): :construction: work in process and fix to send file

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import type { Metadata, Viewport } from "next";
 import { Footer } from "@/components/Footer";
 import { Inter } from "next/font/google";
 import { Navbar } from "@/components/Navbar";
+import { Toaster } from "@/components/ui/toaster"
 
 const inter = Inter({ subsets: ["latin"] });
 export const viewport: Viewport = {
@@ -29,8 +30,13 @@ export default function RootLayout({
     <html lang="en" >
       <body className={inter.className} suppressHydrationWarning>
         <Navbar />
-        {children}
-        <Footer />
+        <main className="flex flex-col min-h-[calc(100vh-3.5rem-1px)]">
+          <div className="flex-1 flex flex-col h-full">
+            {children}
+          </div>
+          <Footer />
+        </main>
+        <Toaster />
       </body>
     </html>
   );
